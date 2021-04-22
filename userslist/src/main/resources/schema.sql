9565562a1025b41176eff970267289c15988a1b9
Vicky <test@test.com>
2021-04-22 18:35:10 +0530

    Schema Updated

--- userslist/src/main/resources/schema.sql
+++ userslist/src/main/resources/schema.sql
@@ -2,7 +2,11 @@ create table student
 (
    id integer not null,
    name varchar(255) not null,
+   gender varchar(10),
    email varchar(255) not null,
    dept varchar(50),
+   cgpa	double,
+   placement varchar(25),
+   arrears int,
    primary key(id)
 );
