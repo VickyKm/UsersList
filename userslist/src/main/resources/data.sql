6bc77911af1c67f5c7180f938c80f511fb0af39e
Vicky <test@test.com>
2021-04-22 18:52:55 +0530

    Updated scheme

--- userslist/src/main/resources/data.sql
+++ userslist/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into student
-values(10001,'Ranga', 'ranga@test.com');
+values(10001,'Ranga', 'Male','ranga@test.com' , 'CSE', 7, 'Not Placed', 1);
 
 insert into student
-values(10002,'Ravi', 'ravi@test.com');
+values(10002,'Ravi', 'Male','ravi@test.com', 'EEE', 8.5, 'Placed', 0);
