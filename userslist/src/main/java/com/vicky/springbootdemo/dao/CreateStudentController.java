9f97ce13ec373308c7c768b3f57b98fe3ef76a7d
pranesh <pranesh@gmail.com>
2021-04-27 17:03:11 +0530

    Updated Userlist

--- userslist/src/main/java/com/vicky/springbootdemo/dao/CreateStudentController.java
+++ userslist/src/main/java/com/vicky/springbootdemo/dao/CreateStudentController.java
@@ -1,21 +1,45 @@
 package com.vicky.springbootdemo.dao;
 
+import com.vicky.springbootdemo.pojo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("user")
 public class CreateStudentController {
 	
 	@Autowired
 	CreateStudentDAO createStudentDao;
 	
 	@PostMapping(path = "/createUser")
-	public String createStudent(@RequestParam String id, @RequestParam String name, @RequestParam String email) {
+	public String createStudent(@RequestBody CreateStudent student) {
 		
-		createStudentDao.addUser(id,name,email);
-		return "Student " + name + " added successfully";
+   createStudentDao.addUser(student.getName(),student.getGender(),student.getEmail(),student.getDept(),student.getCgpa(),student.getPlacement(),student.getArrears());
+   
+   return "Student " + student.getName() + " Successfully";	
 	}
+	
+	@DeleteMapping(path="/createUser/{id}")
+	public String  deleteStudent(@PathVariable("id") String id) {
+		
+		createStudentDao.deleteUser(id);
+		return "student " + id + "  deleted  successfully";
+		
+	}
+	
+	@PutMapping(path="/createUser/{id}")
+	public String updateStudent(@RequestBody CreateStudent student,@PathVariable("id") String id) {
+		createStudentDao.updateUser(student.getName(),student.getId());
+		return "student " + student.getName() + " Updated Successfully"; 
+		}
+	
 }
--- userslist/src/main/java/com/vicky/springbootdemo/dao/CreateStudentDAO.java
+++ userslist/src/main/java/com/vicky/springbootdemo/dao/CreateStudentDAO.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.vicky.springbootdemo.pojo.*;
 @Service
 public class CreateStudentDAO {
 
@@ -14,8 +15,32 @@ public class CreateStudentDAO {
 	@Value("${createuser.query}")
 	String insertQuery;
 	
-	public void addUser(String id, String name, String email) {
+	
+
+	
+	public void addUser(String name, String gender, String email, String dept, String cgpa,  String placement, String arrears) {
 		
-		jdbctemplate.update(insertQuery, id,name,email);
+		jdbctemplate.update(insertQuery, name,gender,email,dept,cgpa,placement,arrears);
 	}
+
+
+	@Value("${deleteuser.query}")
+	String deleteQuery;
+
+	public void deleteUser(String id) {
+		
+		jdbctemplate.update(deleteQuery, id);
+		
+	}
+
+	
+	@Value("${updateuser.query}")
+	String updateQuery;
+	
+	public void updateUser(String name,String id) {
+		jdbctemplate.update(updateQuery, name,id);
+		
+	}
+
+	
 }
